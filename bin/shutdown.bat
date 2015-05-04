d8b47f2d3cbbce9784b5883a0fd088a41ca594c8
David A. Parker <parker.david.a@gmail.com>
2015-05-04 16:38:37 -0400

    Added options, fixed lingering DOS window issue

--- bin/shutdown.bat
+++ bin/shutdown.bat
@@ -10,10 +10,39 @@ REM  AUTHOR:
 REM  Dave Parker
 REM
 REM  DATE:  
-REM  May 2, 2015
+REM  May 4, 2015
 REM
 
-goto check_bundled_java
+REM --- Default configuration file
+set DEF_CONFIG_FILE=..\checkvalvechatrelay.properties
+set CONFIG_FILE=%DEF_CONFIG_FILE%
+
+if "%1"=="" goto check_lib_dir
+if "%1"=="--help" goto usage
+if "%1"=="--config" (
+    if "%2"=="" (
+        echo.
+        echo ERROR: The option --config requires a value.
+        goto usage
+    ) else (
+        set CONFIG_FILE=%2
+        shift
+        shift
+        goto check_opts
+    )
+) else (
+    echo.
+    echo ERROR: Invalid option %1
+    goto usage
+)
+
+REM --- Make sure the directory ..\lib exists
+:check_lib_dir
+if not exist ..\lib (
+    goto no_lib_dir
+) else (
+    goto check_bundled_java
+)
 
 REM --- Check for a bundled JRE
 :check_bundled_java
@@ -36,6 +65,18 @@ if errorlevel 0 (
     goto run_program
 )
 
+REM --- Display and error and exit if ..\lib does not exist
+:no_lib_dir
+echo.
+echo ERROR: Could not find the 'lib' directory in the parent folder.
+echo.
+echo Please ensure that this script is being executed from the
+echo <install_dir>\bin folder, where <install_dir> is the base
+echo installation folder of the CheckValve Chat Relay.
+echo.
+pause
+goto exit
+
 REM --- Display an error and exit if Java could not be found
 :no_java
 echo.
@@ -44,12 +85,26 @@ echo.
 echo Please ensure the Java Runtime Environment (JRE) is installed and
 echo the 'java' executable can be found in your PATH.
 echo.
+pause
 goto exit
 
 REM --- Run chatrelayctl to stop the Chat Relay 
 :run_program
 cd ..\lib
-%JAVA_BIN% -jar chatrelayctl.jar stop
+%JAVA_BIN% -jar lib\chatrelayctl.jar --config %CONFIG_FILE% stop
+goto exit
+
+:usage
+echo.
+echo Usage: %0 [--config <file>]
+echo        %0 [--help]
+echo.
+echo Command-line options:
+echo.
+echo   --config <file>     Read config from <file> [default = %DEF_CONFIG_FILE%]
+echo   --help              Show this help text and exit
+echo.
+pause
 goto exit
 
 :exit
--- bin/startup.bat
+++ bin/startup.bat
@@ -10,15 +10,115 @@ REM  AUTHOR:
 REM  Dave Parker
 REM
 REM  DATE:  
-REM  May 2, 2015
+REM  May 4, 2015
 REM
 
-goto check_bundled_java
+REM --- Default configuration file
+set DEF_CONFIG_FILE=..\checkvalvechatrelay.properties
+set CONFIG_FILE=%DEF_CONFIG_FILE%
+
+REM --- Default debugging options
+set DEF_DEBUG_HOST=localhost
+set DEF_DEBUG_PORT=1044
+set DEBUG=0
+set DEBUG_OPTS=
+set DEBUG_HOST=%DEF_DEBUG_HOST%
+set DEBUG_PORT=%DEF_DEBUG_PORT%
+
+REM --- Default minimum heap size for CheckValve Chat Relay
+set DEF_JVM_MIN_MEM=8m
+set JVM_MIN_MEM=%DEF_JVM_MIN_MEM%
+
+REM --- Default maximum heap size for CheckValve Chat Relay
+set DEF_JVM_MAX_MEM=16m
+set JVM_MAX_MEM=%DEF_JVM_MAX_MEM%
+
+REM --- Check for command line options
+:check_opts
+if "%1"=="" goto check_lib_dir
+if "%1"=="--help" goto usage
+if "%1"=="--config" (
+    if "%2"=="" (
+        echo.
+        echo ERROR: The option --config requires a value.
+        goto usage
+    ) else (
+        set CONFIG_FILE=%2
+        shift
+        shift
+        goto check_opts
+    )
+)
+if "%1"=="--debug" (
+        set DEBUG=1
+        shift
+        goto check_opts
+)
+if "%1"=="--debughost" (
+    if "%2"=="" (
+        echo.
+        echo ERROR: The option --debughost requires a value.
+        goto usage
+    ) else (
+        set DEBUG_HOST=%2
+        shift
+        shift
+        goto check_opts
+    )
+)
+if "%1"=="--debugport" (
+    if "%2"=="" (
+        echo.
+        echo ERROR: The option --debugport requires a value.
+        goto usage
+    ) else (
+        set DEBUG_PORT=%2
+        shift
+        shift
+        goto check_opts
+    )
+)
+if "%1"=="--minheap" (
+    if "%2"=="" (
+        echo.
+        echo ERROR: The option --minheap requires a value.
+        goto usage
+    ) else (
+        set JVM_MIN_MEM=%2
+        shift
+        shift
+        goto check_opts
+    )
+)
+if "%1"=="--maxheap" (
+    if "%2"=="" (
+        echo.
+        echo ERROR: The option --maxheap requires a value.
+        goto usage
+    ) else (
+        set JVM_MAX_MEM=%2
+        shift
+        shift
+        goto check_opts
+    )
+)
+
+echo.
+echo ERROR: Invalid option %1
+goto usage
+
+REM --- Make sure the directory ..\lib exists
+:check_lib_dir
+if not exist ..\lib (
+    goto no_lib_dir
+) else (
+    goto check_bundled_java
+)
 
 REM --- Check for a bundled JRE
 :check_bundled_java
-if exist ..\jre\bin\java.exe (
-    set JAVA_BIN=..\jre\bin\java.exe
+if exist ..\jre\bin\javaw.exe (
+    set JAVA_BIN=..\jre\bin\javaw.exe
     goto run_program
 ) else (
     goto check_system_java
@@ -26,30 +126,64 @@ if exist ..\jre\bin\java.exe (
 
 REM --- Check for Java in the system PATH
 :check_system_java
-java.exe -version >NUL 2>NUL
+javaw.exe -version >NUL 2>NUL
 
 if errorlevel 1 (
     goto no_java
 )
 if errorlevel 0 (
-    set JAVA_BIN=java.exe
+    set JAVA_BIN=javaw.exe
     goto run_program
 )
+
+REM --- Display and error and exit if ..\lib does not exist
+:no_lib_dir
+echo.
+echo ERROR: Could not find the 'lib' directory in the parent folder.
+echo.
+echo Please ensure that this script is being executed from the
+echo <install_dir>\bin folder, where <install_dir> is the base
+echo installation folder of the CheckValve Chat Relay.
+echo.
+pause
+goto exit
 
 REM --- Display an error and exit if Java could not be found
 :no_java
 echo.
 echo ERROR: Unable to locate the 'java' executable.
 echo.
-echo Please ensure the Java Runtime Environment (JRE) is installed and
-echo the 'java' executable can be found in your PATH.
+echo Please ensure the Java Runtime Environment (JRE) is installed
+echo and the 'java' executable can be found in your PATH.
 echo.
+pause
 goto exit
 
-REM --- Run chatrelayctl to start the Chat Relay 
+REM --- Run chatrelayctl to start the Chat Relay
 :run_program
 cd ..\lib
-%JAVA_BIN% -jar chatrelayctl.jar start
+start /b JAVA_BIN% -jar chatrelayctl.jar --config %CONFIG_FILE% start
+goto exit
+
+:usage
+echo.
+echo Usage: %0 [--config <file>] [--minheap <size>] [--maxheap <size>] [--debug [--debughost <ip>] [--debugport <port>]]
+echo        %0 [--help]
+echo.
+echo Command-line options:
+echo.
+echo   --config <file>     Read config from <file> [default = %DEF_CONFIG_FILE%]
+echo   --minheap <size>    Set the JVM's minimum heap size to <size> [default = %DEF_JVM_MIN_MEM%]
+echo   --maxheap <size>    Set the JVM's maximum heap size to <size> [default = %DEF_JVM_MAX_MEM%]
+echo   --debug             Enable the Java debugging listener (for use with jdb)
+echo   --debughost <ip>    IP for jdb connections if debugging is enabled [default = %DEF_DEBUG_HOST%]
+echo   --debugport <port>  Port for jdb connections if debugging is enabled [default = %DEF_DEBUG_PORT%]
+echo   --help              Show this help text and exit
+echo.
+echo Note: For --minheap and --maxheap, the <size> value should be a number followed by k, m, or g
+echo       kilobytes, megabytes, or gigabytes, respectively.  (Ex: 1048576k, 1024m, 1g).
+echo.
+pause
 goto exit
 
 :exit
